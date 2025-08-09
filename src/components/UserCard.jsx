fec0f2ae67a5c1e79db8d2f3a19e3c7f6218d699
Praneeth-klh <2210030187@klh.edu.in>
2025-08-10 03:01:13 +0530

    intial html code

---
+++ src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const card = (props) => {
+  return (
+    <div className="user-card">
+        <img src={props.image} alt={`${props.firstname}'s avatar`} />
+        <h1>{props.firstname}{props.lastName}</h1>
+        <p>{props.gender}</p>
+    </div>
+  )
+}
+
+export default card
