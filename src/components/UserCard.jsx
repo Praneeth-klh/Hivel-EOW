ce56f344ad00e6dad6beac729b7e47784d08396e
Praneeth-klh <2210030187@klh.edu.in>
2025-08-10 03:16:22 +0530

    small addons

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -6,6 +6,7 @@ const card = (props) => {
         <img src={props.image} alt={`${props.firstname}'s avatar`} />
         <h1>{props.firstname}{props.lastName}</h1>
         <p>{props.gender}</p>
+        <p>{props.email}</p>
     </div>
   )
 }
