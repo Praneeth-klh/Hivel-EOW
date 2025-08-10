4382549178084935765d36e5ba6d275c3c40f9ce
Praneeth-klh <2210030187@klh.edu.in>
2025-08-10 19:30:25 +0530

    Done with fetching

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
-import Card from './components/UserCard';
 import Search from './components/Search';
 import FullView from './components/FullView';
-
+import USerLists from './components/UserList';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  const getUser = () => {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data.users);
+      })
+      .catch((err) => {
+        console.error("Error fetching users:", err);
+      });
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <div>
       <Search />
-      <Card name="Praneeth" />
-      <FullView/>
+      {/* FIX: changed `user` to `users` */}
+      <USerLists users={users} />  
+      <FullView />
     </div>
   );
 }
